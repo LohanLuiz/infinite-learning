c0d18a9d5d9ee2916b7f4e7a5ebb569c05e235dc
Lohan Luiz <lohanluiz12@gmail.com>
2021-03-10 19:28:27 -0400

    progress: javascript function to switch view

--- 08-sliding-login/script.js
+++ 08-sliding-login/script.js
@@ -1,3 +1,24 @@
 const loginBtn = document.querySelectorAll(".login-btn");
 const registerBtn = document.querySelectorAll(".register-btn");
 const lostPassBtn = document.querySelectorAll(".lost-pass-btn");
+const box = document.querySelector(".box");
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+const lostPasswordForm = document.querySelector(".lost-password-form");
+
+registerBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    box.classList.add("slide-active");
+    registerForm.classList.remove("form-hidden");
+    loginForm.classList.add("form-hidden");
+
+  })
+})
+
+loginBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    box.classList.remove("slide-active");
+    registerForm.classList.add("form-hidden");
+    loginForm.classList.remove("form-hidden");
+  })
+})
